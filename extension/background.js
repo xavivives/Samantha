9f3eacd9c93b8df73b845778327d4f20c1172060
Xavi <xavings@gmail.com>
2016-03-25 02:41:44 +0800

    Suggesting results on omnibox typing!

--- extension/background.js
+++ extension/background.js
@@ -4,6 +4,8 @@ chrome.omnibox.onInputEntered.addListener(onOmniboxEnter);
 
 var clipboard = "";
 
+setOmniboxSuggestion("What are you looking for?");
+
 function onContentConnected ( port )
 {
     port.postMessage("Conected with content");
@@ -25,7 +27,6 @@ function processMessage(event, value)
 function onCopy(str)
 {
     clipboard = str;
-    setOmniboxSuggestion(str);
 }
 
 function onSelected(selectionObj)
@@ -35,16 +36,12 @@ function onSelected(selectionObj)
 
 function onOmniboxEnter(str)
 {
-    var config = 
-    {
-        fields:
-        {
-            url: {boost: 2},
-            content: {boost: 1}
-        }
-    }
+    console.log(results);
+}
 
-    config =
+function getSearchResults(textToSearch)
+{
+    var config =
     {
         fields:
         {
@@ -62,19 +59,48 @@ function onOmniboxEnter(str)
         expand: true
     }
 
-    var results = index.search(str, config);
+    var results = index.search(textToSearch, config);
 
-    console.log(results);
+    return results;
+}
+
+function resultsIntoSuggestions(results, maxResults, minScore)
+{
+    var suggestions = [];
+    for (i = 0; i < results.length; i++)
+    { 
+        if(results[i].score<minScore)
+            return suggestions;
+
+        var entry = index.documentStore.getDoc(results[i].ref);
+
+        var suggestion =
+        {
+            content: entry.url,
+            description: entry.content
+        }
+        suggestions.push(suggestion);
+    }
+    return suggestions;
 }
 
 function goToUrl(url)
 {
     chrome.tabs.update(null, {url:"url"});
 }
+//OMNIBOX
+
+chrome.omnibox.onInputChanged.addListener(onOmniboxInputChanged);
+
+function onOmniboxInputChanged(text, suggest)
+{
+    var results =  getSearchResults(text);
+    var suggestions = resultsIntoSuggestions(results);
+    suggest(suggestions);
+}
 
 function setOmniboxSuggestion(str)
 {
-    console.log(str);
     var obj = {description: str};
     chrome.omnibox.setDefaultSuggestion(obj);
 }
