c3e9d303a8db9fcfa3153ba317c06ad460e94739
Xavi <xavings@gmail.com>
2016-03-26 01:41:16 +0800

    Adding persistency using local storage

--- extension/background.js
+++ extension/background.js
@@ -3,9 +3,12 @@ chrome.runtime.onMessage.addListener(onContentMessage);
 chrome.omnibox.onInputEntered.addListener(onOmniboxEnter);
 chrome.omnibox.onInputChanged.addListener(onOmniboxInputChanged);
 
+var stateConfig = null;
+var index = null;
 var clipboard = "";
-
 setOmniboxSuggestion("What are you looking for?");
+
+start();
 
 function onContentConnected ( port )
 {
@@ -32,7 +35,10 @@ function onCopy(str)
 
 function onSelected(selectionObj)
 {
-    addSearchEntry(selectionObj.url, selectionObj.selectedText);
+    var entry = getNewEntry(selectionObj.url, selectionObj.selectedText);
+    addSearchEntry(entry);
+    saveEnry(entry);
+    saveStateConfig();
 }
 
 function onOmniboxEnter(str)
@@ -48,12 +54,12 @@ function getSearchResults(textToSearch)
         {
             url:
             {
-                boost: 2,
+                boost: 1,
                 bool: "OR"
             },
             content:
             {
-                boost: 1
+                boost: 2
             }
         },
         bool: "OR",
@@ -107,17 +113,23 @@ function setOmniboxSuggestion(str)
 
 //SEARCH
 
-var index = elasticlunr(function () {
-    this.setRef('id');
-    this.addField('url');
-    this.addField('content');
-});
+function initSearch()
+{
+    index = elasticlunr(function () {
+        this.setRef('id');
+        this.addField('url');
+        this.addField('content');
+    });
+    //removes stemmers and no words
+    index.pipeline.reset();
+}
 
-//removes stemmers and no words
-index.pipeline.reset();
+function addSearchEntry(entry)
+{
+    index.addDoc(entry);
+}
 
-
-function addSearchEntry(url, content)
+function getNewEntry(url, content)
 {
     var entry = 
     {
@@ -125,14 +137,85 @@ function addSearchEntry(url, content)
         "url" : url,
         "content" : content
     }
-    console.log(entry);
-    index.addDoc(entry);
-}
 
-var _currentUId = 0;
+    return entry;
+}
 
 function getNewUId()
 {
-    _currentUId ++;
-    return _currentUId;
+    stateConfig.currentUId ++;
+    return stateConfig.currentUId.toString();
 }
+
+function populateSearchIndex()
+{
+    for (var i = 0; i<stateConfig.currentUId; i++)
+    {
+        getEntry(i.toString(), onEntryRetrived);
+        
+    }
+}
+
+
+//STORAGE
+
+function saveEnry(entry)
+{   
+    var obj= {};
+    obj[entry.id] = entry;
+    chrome.storage.local.set(obj, function(){});
+}
+
+function getEntry(id, onEntryRetrivedCallback)
+{
+    chrome.storage.local.get(id, onEntryRetrivedCallback);
+}
+
+function onEntryRetrived(obj)
+{   
+    var entry = obj[Object.keys(obj)[0]];//first object of the object
+    console.log(entry);
+    if(!entry)
+        return;
+    addSearchEntry(entry);
+}
+
+
+
+//CONFIG
+
+function start()
+{
+    //chrome.storage.local.clear();
+    console.log("start");
+    chrome.storage.local.get("stateConfig", onStateConfigRetrivedCallback);
+}
+
+function saveStateConfig()
+{
+    chrome.storage.local.set({ stateConfig: stateConfig }, function(){});
+}
+
+function onStateConfigRetrivedCallback(obj)
+{
+    console.log("onStateConfigRetrivedCallback");
+    console.log(obj);
+    initStateConfig(obj);
+    initSearch();
+    populateSearchIndex();
+}
+
+function initStateConfig(obj)
+{
+    if(obj.stateConfig && obj.stateConfig.currentUId )
+    {
+        stateConfig = obj.stateConfig;
+    }
+    else
+    {
+        stateConfig = {};
+        stateConfig.currentUId = 0;
+    }
+}
+
+
