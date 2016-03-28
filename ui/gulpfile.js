eb1ebf6cb9a0730357bd9f4c93d283ffcab0a4ac
Xavi <xavings@gmail.com>
2016-03-28 16:55:31 +0800

    Attempting to change the build process

--- ui/gulpfile.js
+++ ui/gulpfile.js
@@ -1,80 +1,197 @@
-'use strict';
-var gulp = require('gulp');
-var browserify = require ('browserify');
-var source = require('vinyl-source-stream'); //necessary to make browserfiy return an stream gulp can understand
-var reactify = require('reactify');//to convert FJX to JS
-var del = require('del');//to delete files
-var watchify = require('watchify');// to trigger tasks when something changes
-var runSequence = require('run-sequence'); // to run task syncronusly
-var gutil = require('gulp-util');
-var less = require('gulp-less');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
+var config      = require('./gulpConfig');
 
-//browserify & watchify options
-var options = 
+//Utils
+
+//bundlelogger
+
+/* bundleLogger
+   ------------
+   Provides gulp style logs to the bundle method in browserify.js
+*/
+
+var gutil        = require('gulp-util');
+var prettyHrtime = require('pretty-hrtime');
+var startTime;
+
+var bundlelogger =
 {
-    entries: ['./src/main.js'],
-    transform: ['reactify'],
-    debug: true,
-    cache: {},//for watchify
-    packageCache: {}//for watchify
+  start: function(filepath) {
+    startTime = process.hrtime();
+    gutil.log('Bundling', gutil.colors.green(filepath) + '...');
+  },
+
+  end: function(filepath) {
+    var taskTime = process.hrtime(startTime);
+    var prettyTime = prettyHrtime(taskTime);
+    gutil.log('Bundled', gutil.colors.green(filepath), 'in', gutil.colors.magenta(prettyTime));
+  }
 };
-var b = browserify(options);
-var w = watchify(b);
-w.plugin('minifyify', {
-            map: 'app.js.map',
-            output: '../extension/app.js.map',
-            compress: { // Options passed to Uglify
-                drop_debugger: true,
-                drop_console: true
-            }
-        })
 
+var notify = require("gulp-notify");
 
-function bundle ()
-{
-    return w.bundle()
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('./../extension'))
-}
+var handleErrors = function() {
 
-//converts JSX to JS, wraps all modules in single file in/../extension/bundle.js
-gulp.task('compile', bundle); 
+  var args = Array.prototype.slice.call(arguments);
 
-gulp.task('default', function cb ()
-{
-    //runSequence('clean',['less','compile','copy'],cb);//runs clean first and compile and copy in parallel
-    runSequence('clean','copy','compile',cb);
+  // Send error to notification center with gulp-notify
+  notify.onError({
+    title: "Compile Error",
+    message: "<%= error.message %>"
+  }).apply(this, args);
 
+  // Keep gulp from hanging on this task
+  this.emit('end');
+};
+
+//Require
+/*
+  gulpfile.js
+  ===========
+  Rather than manage one giant configuration file responsible
+  for creating multiple tasks, each task has been broken out into
+  its own file in gulp/tasks. Any files in that directory get
+  automatically required below.
+  To add a new task, simply add a new task file that directory.
+  gulp/tasks/default.js specifies the default set of tasks to run
+  when you run `gulp`.
+*/
+
+/*var requireDir = require('require-dir');
+
+// Require all tasks in gulp/tasks, including subfolders
+requireDir('./gulp/tasks', { recurse: true });
+*/
+
+//browser sync
+var browserSync = require('browser-sync');
+var gulp        = require('gulp');
+//var config      = require('./gulpConfig').browserSync;
+
+gulp.task('browserSync', ['build'], function() {
+  browserSync(config.browserSync);
 });
 
-w.on('update', function () {
-    gutil.log('File changed, compiling...');
-    bundle();
-    gutil.log('Compiled.');
+
+//browserify
+/* browserify task
+   ---------------
+   Bundle javascripty things with browserify!
+   This task is set up to generate multiple separate bundles, from
+   different sources, and to use Watchify when run from the default task.
+   See browserify.bundleConfigs in gulp/config.js
+*/
+
+var browserify   = require('browserify');
+var watchify     = require('watchify');
+//var bundleLogger = require('../util/bundleLogger');
+var gulp         = require('gulp');
+//var handleErrors = require('../util/handleErrors');
+var source       = require('vinyl-source-stream');
+//var config       = require('./gulpConfig').browserify;
+var babelify     = require('babelify');
+
+gulp.task('browserify', function(callback) {
+
+  var bundleQueue = config.browserify.bundleConfigs.length;
+
+  var browserifyThis = function(bundleConfig) {
+
+    var bundler = browserify({
+      // Required watchify args
+      cache: {}, packageCache: {}, fullPaths: false,
+      // Specify the entry point of your app
+      entries: bundleConfig.entries,
+      // Add file extentions to make optional in your requires
+      extensions: config.browserify.extensions,
+      // Enable source maps!
+      debug: config.browserify.debug
+    });
+
+    var bundle = function() {
+      // Log when bundling starts
+      //bundleLogger.start(bundleConfig.outputName);
+      console.log("start: "+bundleConfig.outputName);
+      return bundler
+        .bundle()
+        // Report compile errors
+        .on('error', handleErrors)
+        // Use vinyl-source-stream to make the
+        // stream gulp compatible. Specifiy the
+        // desired output filename here.
+        .pipe(source(bundleConfig.outputName))
+        // Specify the output destination
+        .pipe(gulp.dest(bundleConfig.dest))
+        .on('end', reportFinished);
+    };
+
+    bundler.transform(babelify.configure());
+
+    if (global.isWatching) {
+      // Wrap with watchify and rebundle on changes
+      bundler = watchify(bundler);
+      // Rebundle on update
+      bundler.on('update', bundle);
+    }
+
+    var reportFinished = function() {
+      // Log when bundling completes
+      //bundleLogger.end(bundleConfig.outputName);
+      console.log("End: "+bundleConfig.outputName);
+
+      if (bundleQueue) {
+        bundleQueue--;
+        if (bundleQueue === 0) {
+          // If queue is empty, tell gulp the task is complete.
+          // https://github.com/gulpjs/gulp/blob/master/docs/API.md#accept-a-callback
+          callback();
+        }
+      }
+    };
+
+    return bundle();
+  };
+
+  // Start bundling with Browserify for each bundleConfig specified
+  config.browserify.bundleConfigs.forEach(browserifyThis);
 });
-//w.on('log', gutil.log);
+
+//build
+
+var gulp = require('gulp');
+gulp.task('build', ['browserify', 'markup']);
 
 
-//deletes the /public directory
-gulp.task('clean', function(done) {
-  del(['public'], done);
+//default
+var gulp = require('gulp');
+gulp.task('default', ['watch']);
+
+
+//markup
+var gulp = require('gulp');
+//var config = require('./gulpConfig').markup;
+
+gulp.task('markup', function() {
+  return gulp.src(config.markup.src)
+    .pipe(gulp.dest(config.markup.dest));
 });
 
-//copy all html and css from /src to /public
-gulp.task('copy', function(done) {
-  gulp.src(['./src/*.html','./img/*', './src/*.css'], {})
-  .pipe(gulp.dest('./public'));
+
+//set watch
+var gulp = require('gulp');
+gulp.task('setWatch', function() {
+  global.isWatching = true;
 });
 
-//LESS
-gulp.task('less', function() {
+//watch
+/* Notes:
+   - gulp/tasks/browserify.js handles js recompiling with watchify
+   - gulp/tasks/browserSync.js watches and reloads compiled files
+*/
 
-  return gulp.src('./src/styles.less')
-    .pipe(sourcemaps.init())
-    .pipe(less())
-    .pipe(autoprefixer({cascade: false, browsers: ['last 2 versions']}))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./public'));
+var gulp   = require('gulp');
+//var config = require('./gulpConfig');
+gulp.task('watch', ['setWatch', 'browserSync'], function() {
+  gulp.watch(config.markup.src, ['markup']);
 });
+
+
