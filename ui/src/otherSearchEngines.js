0777ba7cc8f49a37a4a91bec09b2a6ef3cc86035
Xavi <xavings@gmail.com>
2016-04-07 01:50:22 +0800

    Getting google search keywords from hash values

--- ui/src/otherSearchEngines.js
+++ ui/src/otherSearchEngines.js
@@ -12,6 +12,7 @@ export default class OtherSearchEngines
         return false;
     }
 
+    //for urls like this: https://www.google.com.hk/search?q=something+sweet&oq=something&aqs=chrome.1.69i59j0l5.2733j0j9&sourceid=chrome&ie=UTF-8
     getSearchText(url)
     {
         var urlObj = new URL(url);
@@ -19,6 +20,17 @@ export default class OtherSearchEngines
         var parameterKey = EnginesData[domain];
         var params = new URLSearchParams(urlObj.search.slice(1));
         var searchText= params.get(parameterKey);
+        if(!searchText)
+            searchText = this.getSearchTextFromHash(url, parameterKey);
+        return searchText;
+    }
+
+    //for urls like this: https://www.google.com.hk/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=something
+    getSearchTextFromHash(url, parameterKey)
+    {
+        var hash = url.split('#')[1];
+        //assumes one parameter onluy
+        var searchText = hash.split(parameterKey+"?")[1];
         return searchText;
     }
 
